f222115deea27efcb8d2caacc25b665bfcdbf41b
520liuXin <799296010@qq.com>
2019-03-23 20:30:16 +0800

    test

--- src/main/java/com/example/cy/config/RedisSessionConfig.java
+++ src/main/java/com/example/cy/config/RedisSessionConfig.java
@@ -1,6 +1,9 @@
 package com.example.cy.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
@@ -9,4 +12,11 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @Configuration
 @EnableRedisHttpSession
 public class RedisSessionConfig {
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        final RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+
+    }
 }

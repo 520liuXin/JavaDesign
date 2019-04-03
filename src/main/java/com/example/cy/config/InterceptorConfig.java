0c0f63b2d48205a9a62157b7ea1bc5d17df8621f
520liuXin <799296010@qq.com>
2019-04-03 19:13:58 +0800

    liuxin;

--- src/main/java/com/example/cy/config/InterceptorConfig.java
+++ src/main/java/com/example/cy/config/InterceptorConfig.java
@@ -1,16 +1,38 @@
-//package com.example.cy.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
-//import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-//
-//import java.util.List;
-//
-//@Configuration
-//public class InterceptorConfig extends WebMvcConfigurationSupport {
-//    @Override
-//    protected void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-//        argumentResolvers.add(new PageableHandlerMethodArgumentResolver());
-//    }
-//}
+package com.example.cy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
+
+@Configuration
+public class InterceptorConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        super.addInterceptors(registry);
+        registry.addInterceptor(new MyIntercept()).addPathPatterns("/*");
+    }
+
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        super.addResourceHandlers(registry);
+        //将默认配置的 /** 映射到 /static （或/public、/resources、/META-INF/resources）
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/resources/")
+                .addResourceLocations("classpath:/static/");
+
+    }
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        // 注册Spring data jpa pageable的参数分解器
+        argumentResolvers.add(new PageableHandlerMethodArgumentResolver());
+
+    }
+}

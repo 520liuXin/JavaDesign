86c92db875a7ebb8975d089860602883f413ee33
520liuXin <799296010@qq.com>
2019-04-23 11:18:13 +0800

    登录验证码

--- src/main/java/com/example/cy/controller/LoginController.java
+++ src/main/java/com/example/cy/controller/LoginController.java
@@ -8,6 +8,7 @@ import com.example.cy.security.SecurityUtils;
 import com.example.cy.service.impl.UserServiceImpl;
 import com.example.cy.utils.ResponseInfo;
 import com.example.cy.utils.StringUtils;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
 import io.netty.util.internal.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,24 +18,40 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
 @Controller
 public class LoginController {
 
     @Autowired
     private AuthenticationManager myAuthenticationManager;
 
+    @Autowired
+    DefaultKaptcha defaultKaptcha;
+
+   private String CHECK_CODE = "CHECK_CODE";
     @ResponseBody
     @PostMapping("/userLogin")
     public ResponseInfo<?> userLogin(HttpServletRequest request, @RequestBody JSONObject params) {
 
 
+
         String username=params.getString("name");
         String password=params.getString("pwd");
+        String kaptcha=params.getString("kaptcha");
+
+        String s = request.getSession().getAttribute(CHECK_CODE).toString();
+        if (StringUtils.isEmpty(kaptcha) || !s.equals(kaptcha)) {
+            return ResponseInfo.error("验证码不正确");
+        }
         System.out.println("username:"+username+"------password:"+password);
         UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(username, password);
-
 
         try{
             //使用SpringSecurity拦截登陆请求 进行认证和授权
@@ -62,4 +79,24 @@ public class LoginController {
     }
 
 
+    @RequestMapping("/captcha.jpg")
+    @ResponseBody
+    public ResponseInfo applyCheckCode(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+
+        response.setHeader("Cache-Control", "no-store, no-cache");
+        response.setContentType("image/jpeg");
+
+        //生成文字验证码
+        String text = defaultKaptcha.createText();
+        //生成图片验证码
+        BufferedImage image = defaultKaptcha.createImage(text);
+        //保存到session
+        request.getSession().setAttribute(CHECK_CODE, text);
+        ServletOutputStream out = response.getOutputStream();
+        ImageIO.write(image, "jpg", out);
+        return ResponseInfo.success("");
+    }
 }
+
+
